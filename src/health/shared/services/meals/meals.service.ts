461520e03c1959cf9f41131b890cfa94902d5a1a
velikovmartin <velikovmartin@yahoo.com>
2018-07-02 13:15:53 +0300

    Reactive Form outputs and async / await

--- src/health/shared/services/meals/meals.service.ts
+++ src/health/shared/services/meals/meals.service.ts
@@ -1,7 +1,6 @@
-import { Meal } from './meals.service';
-import { AuthService } from './../../../../auth/shared/services/auth/auth.service';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 import { Store } from 'store';
 
 import { Observable } from 'rxjs/Observable';
@@ -10,42 +9,49 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 
+import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
+
 export interface Meal {
-    name: string,
-    ingredients: string[],
-    timestamp: number,
-    $key: string,
-    $exists: () => boolean
+  name: string,
+  ingredients: string[],
+  timestamp: number,
+  $key: string,
+  $exists: () => boolean
 }
 
 @Injectable()
 export class MealsService {
 
-    meals$: Observable<Meal[]> = this.db.list(`meals/${this.uid}`)
-        .do(next => this.store.set('meals', next))
+  meals$: Observable<Meal[]> = this.db.list(`meals/${this.uid}`)
+    .do(next => this.store.set('meals', next));
 
-    constructor(
-        private store: Store,
-        private db: AngularFireDatabase,
-        private authService: AuthService
-    ) {}
+  constructor(
+    private store: Store,
+    private db: AngularFireDatabase,
+    private authService: AuthService
+  ) {}
 
-    get uid() {
-        return this.authService.user.uid;
-    }
+  get uid() {
+    return this.authService.user.uid;
+  }
 
-    getMeal(key: string) {
-        if (!key) return Observable.of({});
-        return this.store.select<Meal[]>('meals')
-          .filter(Boolean)
-          .map(meals => meals.find((meal: Meal) => meal.$key === key));
-    }
+  getMeal(key: string) {
+    if (!key) return Observable.of({});
+    return this.store.select<Meal[]>('meals')
+      .filter(Boolean)
+      .map(meals => meals.find((meal: Meal) => meal.$key === key));
+  }
 
-    addMeal(meal: Meal) {
-        return this.db.list(`meals/${this.uid}`).push(meal);
-    }
+  addMeal(meal: Meal) {
+    return this.db.list(`meals/${this.uid}`).push(meal);
+  }
 
-    removeMeal(key: string) {
-        return this.db.list(`meals/${this.uid}`).remove(key);
-    }
+  updateMeal(key: string, meal: Meal) {
+    return this.db.object(`meals/${this.uid}/${key}`).update(meal);
+  }
+
+  removeMeal(key: string) {
+    return this.db.list(`meals/${this.uid}`).remove(key);
+  }
+
 }
